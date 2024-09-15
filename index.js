422d8de8f95c1274a33a490a2ba88010a1cd1499
May <abeillee@bk.ru>
2024-09-15 15:34:13 +0300

    fix: fix burger position

--- index.js
+++ index.js
@@ -3,29 +3,36 @@ const headerNav = document.querySelector('.header-nav');
 const burgerWrapper = document.querySelector('.burger-wrapper');
 const headerNavList = document.querySelector('.header-nav__list');
 
+const togglerOpen = (element) => {
+    element.classList.toggle('open');
+}
+
+const togglerRemove = (element) => {
+    element.classList.remove('open');
+}
 
 btnBurger.addEventListener('click', (event) => {
-    btnBurger.classList.toggle('open');
-    headerNav.classList.toggle('open');
-    burgerWrapper.classList.toggle('open');
+    togglerOpen(btnBurger);
+    togglerOpen(headerNav);
+    togglerOpen(burgerWrapper);
     document.body.classList.toggle('hidden');
-    }
+}
 )
 
-
 headerNavList.addEventListener('click', (event) => {
-if (event.target.classList.contains('header-nav__list') || (event.target.classList.contains('list-link'))) {
-    btnBurger.classList.remove('open');
-    headerNav.classList.remove('open');
-    burgerWrapper.classList.remove('open');
-    document.body.classList.remove('hidden');
-}
+    console.log(event.target);
+    if (event.target.classList.contains('header-nav__list') || (event.target.classList.contains('list-link'))) {
+        togglerRemove(btnBurger);
+        togglerRemove(headerNav);
+        togglerRemove(burgerWrapper);
+        document.body.classList.remove('hidden');
+    }
 })
 
 burgerWrapper.addEventListener('click', (event) => {
-    btnBurger.classList.remove('open');
-    headerNav.classList.remove('open');
-    burgerWrapper.classList.remove('open');
+    togglerRemove(btnBurger);
+    togglerRemove(headerNav);
+    togglerRemove(burgerWrapper);
     document.body.classList.remove('hidden');
 })
 
