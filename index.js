eeed11a904f00749ce8028a1b232f5d4fd2692e6
May <abeillee@bk.ru>
2024-09-20 21:57:50 +0300

    add: add carousel

--- index.js
+++ index.js
@@ -2,7 +2,73 @@ const btnBurger = document.querySelector('.header-burger');
 const headerNav = document.querySelector('.header-nav');
 const burgerWrapper = document.querySelector('.burger-wrapper');
 const headerNavList = document.querySelector('.header-nav__list');
+// Карусель
+const BTN_LEFT_CAROUSEL = document.querySelector('.arrow-left--slider');
+const BTN_RIGHT_CAROUSEL = document.querySelector('.arrow-right--slider');
+const CARDS = document.querySelectorAll('.card');
+const CAROUSEL = document.querySelector('.our-friends-cards__carousel');
+const CARDS_LEFT = document.querySelector('.cards--left');
+const CARDS_RIGHT = document.querySelector('.cards--right');
+const CARDS_ACTIVE = document.querySelector('.cards--active');
+const CARDS_ACTIVE_CHILDREN = CARDS_ACTIVE.children;
 
+
+
+
+const cardsArray = [
+    {
+        id: 0,
+        name: "Katrine",
+        img: "images/pets-katrine.jpg",
+        btn: "Learn more"
+    },
+    {
+        id: 1,
+        name: "Jennifer",
+        img: "images/pets-jennifer.jpg",
+        btn: "Learn more"
+    },
+    {
+        id: 2,
+        name: "Woody",
+        img: "images/pets-woody.jpg",
+        btn: "Learn more"
+    },
+    {
+        id: 3,
+        name: "Sophia",
+        img: "images/pets-sophia.jpg",
+        btn: "Learn more"
+    },
+    {
+        id: 4,
+        name: "Timmy",
+        img: "images/pets-timmy.jpg",
+        btn: "Learn more"
+    },
+    {
+        id: 5,
+        name: "Charly",
+        img: "images/pets-charly.jpg",
+        btn: "Learn more"
+    },
+    {
+        id: 6,
+        name: "Scarlett",
+        img: "images/pets-scarlet.jpg",
+        btn: "Learn more"
+    },
+    {
+        id: 7,
+        name: "Freddie",
+        img: "images/pets-freddie.jpg",
+        btn: "Learn more"
+    }
+]
+
+
+
+// Интерактив для бургера
 const togglerOpen = (element) => {
     element.classList.toggle('open');
 }
@@ -37,7 +103,78 @@ burgerWrapper.addEventListener('click', (event) => {
 })
 
 
+// Карусель
 
+const moveLeft = () => {
+    CAROUSEL.classList.add('transition-left');
+    BTN_LEFT_CAROUSEL.removeEventListener('click', (moveLeft));
+    BTN_RIGHT_CAROUSEL.removeEventListener('click', (moveRight));
+}
+
+const moveRight = () => {
+    CAROUSEL.classList.add('transition-right');
+    BTN_RIGHT_CAROUSEL.removeEventListener('click', (moveRight));
+    BTN_LEFT_CAROUSEL.removeEventListener('click', (moveLeft));
+}
+
+const createCardTemplate = () => {
+    const card1 = document.createElement('div');
+    card1.classList.add('card');
+    return card1;
+}
+
+BTN_LEFT_CAROUSEL.addEventListener('click', (moveLeft));
+BTN_RIGHT_CAROUSEL.addEventListener('click', (moveRight));
+
+
+CAROUSEL.addEventListener('animationend', (animationEvent) => {
+    let changedItem;
+
+    if (animationEvent.animationName === 'move-left') {
+        CAROUSEL.classList.remove('transition-left');
+        changedItem = CARDS_LEFT;
+        document.querySelector('.cards--active').innerHTML = CARDS_LEFT.innerHTML;
+
+    } else if (animationEvent.animationName === 'move-right') {
+        CAROUSEL.classList.remove('transition-right');
+        changedItem = CARDS_RIGHT;
+        CAROUSEL.classList.remove('transition-left');
+        document.querySelector('.cards--active').innerHTML = CARDS_RIGHT.innerHTML;
+    }
+
+    changedItem.innerHTML = '';
+
+    // for (let i = 0; i < 3; i++) {
+    //     const cardNew = createCardTemplate();
+    //     cardNew.innerText = Math.floor(Math.random() * 8);
+    //     changedItem.appendChild(cardNew);
+    // }
+
+    let activeIndexes = Array.from(CARDS_ACTIVE_CHILDREN).map((item) => item.getAttribute('id'));
+    let newIndexes = [];
+
+    while (newIndexes.length < 3) {
+        const cardNew = createCardTemplate();
+        const indexRandom = String(Math.floor(Math.random() * 8));
+
+        if (!activeIndexes.includes(indexRandom) && (!newIndexes.includes(indexRandom))) {
+            newIndexes.push(indexRandom);
+        }
+    }
+
+    for (i of newIndexes) {
+        const cardNew = createCardTemplate();
+        cardNew.setAttribute('id', i);
+        cardNew.innerHTML = `<img src="${cardsArray[+i].img}" alt="${cardsArray[+i].name}" class="card__img">
+        <div class="card__name">${cardsArray[+i].name}</div>
+        <button class="card__button button-outline button">${cardsArray[i].btn}</button>`;
+        changedItem.appendChild(cardNew);
+        console.log(cardNew);
+    }
+
+    BTN_LEFT_CAROUSEL.addEventListener('click', (moveLeft));
+    BTN_RIGHT_CAROUSEL.addEventListener('click', (moveRight));
+})
 
 
 
